6828351e5ff3dc0c1fa0860df2853ee218ab67a6
chiranjeebnayak <chiranjeebnayak.37@gmail.com>
2024-07-30 21:45:39 +0530

    till Reducer

--- src/components/Reduce.jsx
+++ src/components/Reduce.jsx
@@ -1,30 +1,45 @@
-import { useReducer, useState } from 'react';
+import { useReducer, useState } from "react";
 
-const initialState = {age: 10};
-// function reducer(state, action) {
-//   console.log(JSON.stringify(state));
-//   console.log(JSON.stringify(action));
-//   if (action.type === 'incremented_age') {
-//     return {
-//       age: state.age + 1
-//     };
-//   }
-// }
+const initialState = { age: 10 };
+function reducer(state, action) {
+  console.log(JSON.stringify(state));
+  console.log(JSON.stringify(action));
+  if (action.type === "age_badhao") {
+    return {
+      age: state.age + 1,
+    };
+  }
+  if (action.type === "age_kamao") {
+    return {
+      age: state.age - 1,
+    };
+  }
+}
 
 function Reducer() {
-  // const [state, dispatch] = useReducer(reducer, initialState);
-  const [age, setAge] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [age, setAge] = useState(0)
   return (
     <>
-      {/* <button onClick={() => {
-        dispatch({ type: 'incremented_age' })
-      }}>
+      <button
+        onClick={() => {
+          dispatch({ type: "age_badhao" });
+        }}
+      >
         Increment age
-      </button> */}
-  <button onClick={() => setAge(age + 1)}> Increment age</button>
-      <p>Hello! You are {age}.</p>
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "age_kamao" });
+        }}
+      >
+        Decrement age
+      </button>
+      {/* <button onClick={() => setAge(age + 1)}> Increment age</button> */}
+      <p>Hello! You are {state.age}.</p>
     </>
   );
 }
 
-export default Reducer
+export default Reducer;
